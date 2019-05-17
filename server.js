56e0a701fa6852044fa085756cf2bdfe76970a51
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-05-16 21:45:16 -0400

    js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+var path = require("path");
+//var data = require("data-service.js");
+var employees = require("employees.json");
 
 app.use(express.static('public')); 
 
@@ -13,6 +16,11 @@ app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
+app.get("/employees", (req, res) => {
+    //res.send(employees);
+    //res.json(employees);
+});
+
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
-
+console.log(`Express http server listening on ${HTTP_PORT}`);
