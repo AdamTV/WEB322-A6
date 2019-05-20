fd3873600aa61f3cc774bdc13b50c61263678199
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-05-20 15:17:30 -0400

    dsjs

--- data-service.js
+++ data-service.js
@@ -4,15 +4,15 @@ var departments = [];
 
 module.exports.initialize = function () {
   return new Promise(function (resolve, reject) {
-    fs.readFile('/data/employees', 'utf8', (err, data) => {
-      if (err) reject("unable to read file");
-      console.log(data);
-      JSON.parse(data);
-      employees.push(data);
-      fs.readFile('/data/departments', 'utf8', (err, data) => {
-        if (err) reject("unable to read file");
-        JSON.parse(data);
-        departments.push(data);
+    fs.readFile('data/employees.json', 'utf8', (err, data) => {
+      if (err) reject("unable to read employee file");
+      obj = JSON.parse(data);
+      employees = obj;
+      fs.readFile('data/departments.json', 'utf8', (err, data) => {
+        if (err) reject("unable to read departments file");
+        console.log(data);
+        obj = JSON.parse(data);
+        departments = obj;
         resolve();
       });
     });
