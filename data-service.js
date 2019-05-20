aaf55e82bc3f980656ece59c0f3abe7f3dd1b34f
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-05-20 14:41:49 -0400

    js

--- data-service.js
+++ data-service.js
@@ -2,32 +2,21 @@ const fs = require('fs');
 var employees = [];
 var departments = [];
 
-module.exports.initialize = function() {
-fs.readFile('/data/employees','utf8', (err, data) => {
-    if (err) Promise.reject(new Error('unable to read file')).then(function() {
-        // not called
-      }, function(error) {
-        console.log(error); // Stacktrace
-      });
-      else{
-    JSON.parse(data);
-    employees.push(data);
-    Promise.resolve();
-      }
+module.exports.initialize = function () {
+  fs.readFile('/data/employees', 'utf8', (err, data) => {
+    if (err) return Promise.reject(new Error('unable to read employee file'));
+    else {
+      JSON.parse(data);
+      employees.push(data);
+    }
   });
 
-  fs.readFile('/data/departments','utf8', (err, data) => {
-    if (err){
-        Promise.reject(new Error('unable to read file')).then(function() {
-            // not called
-          }, function(error) {
-            console.log(error); // Stacktrace
-          });
-    }
-    else{
-    JSON.parse(data);
-    departments.push(data);
-    Promise.resolve();
+  fs.readFile('/data/departments', 'utf8', (err, data) => {
+    if (err) return Promise.reject(new Error('unable to read departments file'));
+    else {
+      JSON.parse(data);
+      departments.push(data);
     }
   });
+  return new Promise.resolve();
 }
